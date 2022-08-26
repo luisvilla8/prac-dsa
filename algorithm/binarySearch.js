e080615594395704f17f53caa839893a25dfd077
Luis Villacorta <85130000+luisvilla8@users.noreply.github.com>
2022-08-25 23:43:38 -0500

    feat:Add recursive binary search

--- algorithm/binarySearch.js
+++ algorithm/binarySearch.js
@@ -17,3 +17,23 @@ const binarysearch = (arr, target) => {
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const target = 5;
 console.log(binarysearch(array, target));
+******************************************
+RECURSIVE
++++++++++++
+const binarysearch = (arr, target, low, high) => {
+  if (low <= high) {
+    let mid = Math.floor(low + high / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] > target) {
+      return binarysearch(arr, target, low, mid - 1);
+    }
+    if (arr[mid] < target) {
+      return binarysearch(arr, target, mid + 1, high);
+    }
+  }
+  return -1;
+};
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const target = 4;
+console.log(binarysearch(array, target, 0, 9));
