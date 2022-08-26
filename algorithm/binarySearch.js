ad2a02abd5708c16cdee6cda6c2ff9312e7c94d8
Luis Villacorta <85130000+luisvilla8@users.noreply.github.com>
2022-08-26 00:01:17 -0500

    fix:Change mid variable declaration of recursive binary search

--- algorithm/binarySearch.js
+++ algorithm/binarySearch.js
@@ -22,7 +22,8 @@ RECURSIVE
 +++++++++++
 const binarysearch = (arr, target, low, high) => {
   if (low <= high) {
-    let mid = Math.floor(low + high / 2);
+    //let mid = Math.floor(low + high / 2);
+    let mid = low + Math.floor((high - low) / 2);
     if (arr[mid] === target) return mid;
     if (arr[mid] > target) {
       return binarysearch(arr, target, low, mid - 1);
